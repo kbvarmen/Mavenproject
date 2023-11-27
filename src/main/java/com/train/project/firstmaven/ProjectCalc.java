b23324a9e70a2a79dfaf9e00ec1c5aac797d94c7
Kabilavarmen L <kbvarmen@users.noreply.github.com>
2023-11-27 19:31:40 +0530

    Initial commit

---
+++ src/main/java/com/train/project/firstmaven/ProjectCalc.java
@@ -0,0 +1,21 @@
+package com.train.project.firstmaven;
+
+public class ProjectCalc {
+
+	
+	
+public int addnum(int num1,int num2) {
+	
+	
+	return 4+5;
+	
+}
+	
+public int subnum(int num1,int num2) {
+	
+	
+	return num1-num2;
+	
+}
+
+}
---
+++ src/test/java/com/train/project/firstmaven/Projectcalctest.java
@@ -0,0 +1,19 @@
+package com.train.project.firstmaven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Projectcalctest {
+
+	@Test
+	public void testAddnum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubnum() {
+		fail("Not yet implemented");
+	}
+
+}
